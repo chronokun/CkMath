4dae8ad60e731419d9b5a8da27b87f1e2de4f5e0
chronokun <chronokun@hotmail.com>
2015-02-16 16:32:08 +1300

    Updated geometry tests.

--- CkMath/tests_geometry.cpp
+++ CkMath/tests_geometry.cpp
@@ -16,7 +16,14 @@ const bool Test_Geometry_GetInterval()
 	const TRay3d kRay3d{	TVector3d{0.0, 0.0, 0.0},
 							TVector3d{1.0, 0.0, 0.0}};
 
-	const bool kPass3d = Equal(GetInterval(kRay3d, kPlane3d), 2.0, s_kdEpsilon);
+	const TPlane3f kPlane3f{	TVector3f{2.0f, 0.0f, 0.0f},
+								TVector3f{-1.0f, 0.0f, 0.0f}};
 
-	return(kPass3d);
+	const TRay3f kRay3f{	TVector3f{0.0f, 0.0f, 0.0f},
+							TVector3f{1.0f, 0.0f, 0.0f}};
+
+	const bool kPass3d = Equal(GetInterval(kRay3d, kPlane3d), 2.0, s_kdEpsilon);
+	const bool kPass3f = Equal(GetInterval(kRay3f, kPlane3f), 2.0f, s_kfEpsilon);
+
+	return(kPass3d && kPass3f);
 }
